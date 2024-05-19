dc724df1ea46301d8c77acc75cf2e1abb701db36
wuchao <wuchao@apusapps.com>
2024-05-19 23:19:38 +0800

    基于Opacity 与 NotificationListener 实现AppBar滚动渐变效果

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -10,41 +10,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin {
+  static const appbarScrollOffset = 100;
   final bannerList = [
     "https://www.devio.org/io/flutter_app/img/banner/100h10000000q7ght9352.jpg",
     "https://o.devio.org/images/fa/cat-4098058__340.webp",
     "https://o.devio.org/images/fa/photo-1601513041797-a79a526a521e.webp",
     "https://o.devio.org/images/other/as-cover.png",
   ];
+  double appBarAlpha = 0;
+
   get _logoutBtn => TextButton(
         onPressed: () {
           LoginDao.logOut();
         },
         child: const Text(
           "登出",
-          style: TextStyle(color: Colors.white),
         ),
       );
+
+  get _appBar => Opacity(
+        opacity: appBarAlpha,
+        child: Container(
+          height: 80,
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(color: Colors.white),
+          child: const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text('首页'),
+          ),
+        ),
+      );
+
+  get _listView => ListView(
+        children: [
+          BannerWidget(bannerList: bannerList),
+          const SizedBox(
+            height: 800,
+            child: ListTile(
+              title: Text('哈哈'),
+            ),
+          )
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        title: const Text(
-          "首页",
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: [
-          _logoutBtn,
-        ],
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          BannerWidget(
-            bannerList: bannerList,
-          )
+          MediaQuery.removePadding(
+            // 移除顶部空白
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+                  ///通过depth来过滤指定widget发出的滚动事件，depth == 0表示最外层的列表发出的滚动事件滚动且是列表滚动的事件
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+                return false;
+              },
+              child: _listView,
+            ),
+          ),
+          _appBar,
         ],
       ),
     );
@@ -52,4 +82,19 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
 
   @override
   bool get wantKeepAlive => true;
+
+  void _onScroll(double offset) {
+    print('offset:$offset');
+    double alpha = offset / appbarScrollOffset;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    print('alpha:$alpha');
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print('appBarAlpha:$appBarAlpha');
+  }
 }
